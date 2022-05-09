0cd7330397294f9dd2e6c66cf95e2279376b01e7
Valentin Sasyan <valentin.sasyan@ign.fr>
2022-05-09 17:41:27 +0200

    Ajout des interfaces

---
+++ tests/store/CommentInterfaceTestCase.py
@@ -0,0 +1,20 @@
+import unittest
+
+
+class CommentInterfaceTestCase(unittest.TestCase):
+    """Tests CommentInterface class.
+
+    cmd : python3 -m unittest -b tests.store.CommentInterfaceTestCase
+    """
+
+    def test_api_add_comment(self) -> None:
+        "Vérifie le bon fonctionnement de api_add_comment."
+
+    def test_api_list_comments(self) -> None:
+        "Vérifie le bon fonctionnement de api_list_comments."
+
+    def test_api_edit_comment(self) -> None:
+        "Vérifie le bon fonctionnement de api_edit_comment."
+
+    def test_api_remove_comment(self) -> None:
+        "Vérifie le bon fonctionnement de api_remove_comment."
