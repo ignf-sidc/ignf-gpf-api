814d612ba86b51add2be39d0fcaa8f72deaaab5f
Ludivine Schlegel <ludivine.schlegel@ign.fr>
2022-05-20 16:21:44 +0200

    WorkflowTestCase: supp des exception pylint

--- tests/action/WorkflowTestCase.py
+++ tests/action/WorkflowTestCase.py
@@ -11,13 +11,6 @@ from ignf_gpf_api.action.OfferingAction import OfferingAction
 from ignf_gpf_api.action.ProcessingExecutionAction import ProcessingExecutionAction
 
 
-# pylint:disable=too-many-arguments
-# pylint:disable=too-many-locals
-# pylint:disable=too-many-branches
-# fmt: off
-# (on désactive le formatage en attendant Python 3.10 et la possibilité de mettre des parenthèses pour gérer le multi with proprement)
-
-
 class WorkflowTestCase(unittest.TestCase):
     """Tests UploadAction class.
 
@@ -25,38 +18,40 @@ class WorkflowTestCase(unittest.TestCase):
     """
 
     def test_get_raw_dict(self) -> None:
-        """test de get_raw_dict
-        """
-        d_workflow ={"test": "val"}
+        """test de get_raw_dict"""
+        d_workflow = {"test": "val"}
         o_workflow = Workflow("nom", d_workflow)
         self.assertDictEqual(d_workflow, o_workflow.get_raw_dict())
 
     def test_run_step(self) -> None:
-        """test de run_step
-        """
-        d_workflow = {"workflow": {
-            "steps": [{
-                "name":"1er etape"
-            },{
-                "name":"autre"
-            },{
-                "name": "mise-en-base",
-                "actions": [
+        """test de run_step"""
+        d_workflow = {
+            "workflow": {
+                "steps": [
+                    {"name": "1er etape"},
+                    {"name": "autre"},
                     {
-                        "type": "action1",
-                    }
-                ]
-            },{
-                "name": "mise-en-base2",
-                "actions": [
+                        "name": "mise-en-base",
+                        "actions": [
+                            {
+                                "type": "action1",
+                            }
+                        ],
+                    },
                     {
-                        "type": "action2-1",
-                    },{
-                        "type": "action2-2",
-                    }
+                        "name": "mise-en-base2",
+                        "actions": [
+                            {
+                                "type": "action2-1",
+                            },
+                            {
+                                "type": "action2-2",
+                            },
+                        ],
+                    },
                 ]
-            }]
-        }}
+            }
+        }
         o_workflow = Workflow("nom", d_workflow)
         # l'étape n'existe pas : ça plante.
         with self.assertRaises(WorkflowError) as o_arc:
@@ -67,11 +62,11 @@ class WorkflowTestCase(unittest.TestCase):
         o_mock_action.resolve.return_value = None
         o_mock_action.run.return_value = None
         # on mock Workflow.generate
-        with patch.object(Workflow, "generate", return_value=o_mock_action) as o_mock_action_generate :
+        with patch.object(Workflow, "generate", return_value=o_mock_action) as o_mock_action_generate:
             # test pour une action
             o_workflow.run_step("mise-en-base")
             o_mock_action.run.assert_called_once()
-            o_mock_action_generate.assert_called_once_with('mise-en-base/action1', {'type': 'action1'}, None)
+            o_mock_action_generate.assert_called_once_with("mise-en-base/action1", {"type": "action1"}, None)
             o_mock_action.resolve.assert_called_once_with()
             o_mock_action.run.assert_called_once_with()
 
@@ -82,8 +77,8 @@ class WorkflowTestCase(unittest.TestCase):
             # test pour 2 actions
             o_workflow.run_step("mise-en-base2")
             self.assertEqual(o_mock_action_generate.call_count, 2)
-            o_mock_action_generate.assert_any_call('mise-en-base2/action2-1', {'type': 'action2-1'}, None)
-            o_mock_action_generate.assert_any_call('mise-en-base2/action2-2', {'type': 'action2-2'}, o_mock_action)
+            o_mock_action_generate.assert_any_call("mise-en-base2/action2-1", {"type": "action2-1"}, None)
+            o_mock_action_generate.assert_any_call("mise-en-base2/action2-2", {"type": "action2-2"}, o_mock_action)
             self.assertEqual(o_mock_action.resolve.call_count, 2)
             self.assertEqual(o_mock_action.run.call_count, 2)
 
@@ -98,12 +93,13 @@ class WorkflowTestCase(unittest.TestCase):
         """
         # mock des fonction __init__ des classes action généré
         def new_init(workflow_context: str, definition_dict: Dict[str, Any], parent_action: Optional[ActionAbstract] = None) -> None:
-            print ("new - ", workflow_context, definition_dict, parent_action)
-        d_mock={}
+            print("new - ", workflow_context, definition_dict, parent_action)
 
-        with patch.object(ProcessingExecutionAction, "__init__",  wraps=new_init) as d_mock["ProcessingExecutionAction"], \
-            patch.object(ConfigurationAction, "__init__",  wraps=new_init) as d_mock["ConfigurationAction"], \
-            patch.object(OfferingAction, "__init__",  wraps=new_init) as d_mock["OfferingAction"]:
+        d_mock = {}
+
+        with patch.object(ProcessingExecutionAction, "__init__", wraps=new_init) as d_mock["ProcessingExecutionAction"], patch.object(ConfigurationAction, "__init__", wraps=new_init) as d_mock[
+            "ConfigurationAction"
+        ], patch.object(OfferingAction, "__init__", wraps=new_init) as d_mock["OfferingAction"]:
 
             # exécution
             o_action_generated = Workflow.generate(name, dico_def, parent)
@@ -117,8 +113,7 @@ class WorkflowTestCase(unittest.TestCase):
                     o_mock.assert_not_called()
 
     def test_generate(self) -> None:
-        """test de generate
-        """
+        """test de generate"""
         # mock pour les parents
         o_mock_parent = MagicMock()
 
