ccb74d1108ecad1544570a73f8c04051e23ef81c
Ludivine Schlegel <ludivine.schlegel@ign.fr>
2022-05-20 16:21:26 +0200

    ActionAbstractTestCase: supp des exception pylint

--- tests/action/ActionAbstractTestCase.py
+++ tests/action/ActionAbstractTestCase.py
@@ -6,13 +6,6 @@ from ignf_gpf_api.action.ActionAbstract import ActionAbstract
 from ignf_gpf_api.action.GlobalResolver import GlobalResolver
 
 
-# pylint:disable=too-many-arguments
-# pylint:disable=too-many-locals
-# pylint:disable=too-many-branches
-# fmt: off
-# (on désactive le formatage en attendant Python 3.10 et la possibilité de mettre des parenthèses pour gérer le multi with proprement)
-
-
 # classe temporaire pour les test : ActionAbstract est abstraite
 class ConcreteAction(ActionAbstract):
     def run(self) -> None:
@@ -25,25 +18,21 @@ class ActionAbstractTestCase(unittest.TestCase):
     cmd : python3 -m unittest -b tests.action.ActionAbstractTestCase
     """
 
-
     def test_workflow_context(self) -> None:
-        """test de workflow_context
-        """
-        d_definition ={"test": "val"}
+        """test de workflow_context"""
+        d_definition = {"test": "val"}
         o_action = ConcreteAction("nom", d_definition, None)
         assert "nom" == o_action.workflow_context
 
     def test_definition_dict(self) -> None:
-        """test de definition_dict
-        """
-        d_definition ={"test": "val"}
+        """test de definition_dict"""
+        d_definition = {"test": "val"}
         o_action = ConcreteAction("nom", d_definition, None)
         assert d_definition == o_action.definition_dict
 
     def test_parent_action(self) -> None:
-        """test de parent_action
-        """
-        d_definition ={"test": "val"}
+        """test de parent_action"""
+        d_definition = {"test": "val"}
         o_action = ConcreteAction("nom", d_definition, None)
         assert o_action.parent_action is None
         o_mock_parent = MagicMock()
@@ -51,12 +40,11 @@ class ActionAbstractTestCase(unittest.TestCase):
         assert o_mock_parent == o_action.parent_action
 
     def test_resolve(self) -> None:
-        """test de resolve
-        """
-        d_definition ={"test": "val"}
+        """test de resolve"""
+        d_definition = {"test": "val"}
         d_resolved_dico = {"resolved": "val"}
         # on mock GlobalResolver
-        with patch.object(GlobalResolver, "resolve", return_value=str(json.dumps(d_resolved_dico))) as o_mock_resolve :
+        with patch.object(GlobalResolver, "resolve", return_value=str(json.dumps(d_resolved_dico))) as o_mock_resolve:
             o_action = ConcreteAction("nom", d_definition, None)
             o_action.resolve()
             assert o_action.definition_dict == d_resolved_dico
