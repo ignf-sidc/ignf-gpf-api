e539459a080c4888a99f373426bbe5166fdd4e0f
fbenrabah@ <faycal.benrabah@ign.fr>
2022-05-30 10:35:42 +0200

    test unitaire de la classe action.FileResolver

---
+++ tests/action/FileResolverTestCase.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+import unittest
+
+from ignf_gpf_api.action.FileResolver import FileResolver
+
+
+class FileResolverTestCase(unittest.TestCase):
+    """Tests FileResolverTestCase class.
+
+    cmd : python3 -m unittest -b tests.action.FileResolverTestCase
+    """
+
+    file_path = str(Path(__file__).parent.parent / "_data/action/FileResolver")
+
+    s_str_value: str = "contenu du fichier de type str"
+    s_list_value: str = str('["info_1", "info_2"]')
+    s_dict_value: str = str('{"k1":"v1", "k2":"v2"}')
+
+    def test_resolve_str(self) -> None:
+        """Vérifie le bon fonctionnement de la fonction resolve pour un str."""
+        o_file_resolver = FileResolver("file")
+        s_test_str: str = o_file_resolver.resolve(f"str({self.file_path}/text.txt)")
+        self.assertEqual(self.s_str_value, s_test_str)
+
+    def test_resolve_list(self) -> None:
+        """Vérifie le bon fonctionnement de la fonction resolve pour un list."""
+        o_file_resolver = FileResolver("file")
+        s_test_list: str = o_file_resolver.resolve(f"list({self.file_path}/list.json)")
+        self.assertEqual(self.s_list_value, s_test_list)
+
+    def test_resolve_dict(self) -> None:
+        """Vérifie le bon fonctionnement de la fonction resolve pour un dict."""
+        o_file_resolver = FileResolver("file")
+        s_test_dict: str = o_file_resolver.resolve(f"dict({self.file_path}/dict.json)")
+        self.assertEqual(self.s_dict_value, s_test_dict)
