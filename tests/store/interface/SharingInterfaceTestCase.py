0234a062ca4799d581918c47bf2631c7f2bf4834
Francois Bacquelot <francois.bacquelot@ign.fr>
2022-06-01 10:09:22 +0200

    SharingInterfaceTestCase.py : adaptation à la classe SharingInterface

--- tests/store/interface/SharingInterfaceTestCase.py
+++ tests/store/interface/SharingInterfaceTestCase.py
@@ -3,7 +3,7 @@ import requests
 import requests_mock
 from unittest.mock import patch
 from ignf_gpf_api.io.ApiRequester import ApiRequester
-from ignf_gpf_api.store.interface.CommentInterface import CommentInterface
+from ignf_gpf_api.store.interface.SharingInterface import SharingInterface
 
 
 class SharingInterfaceTestCase(unittest.TestCase):
@@ -20,9 +20,9 @@ class SharingInterfaceTestCase(unittest.TestCase):
         with patch.object(o_api_requester, "route_request", return_value=None) as o_mock_request:
             # On effectue l ajout d'un commentaire
             # On instancie une entité à qui on va ajouter un commentaire
-            o_comment_interface = CommentInterface({"_id": "id_entité"})
+            o_sharing_interface = SharingInterface({"_id": "id_entité"})
             # On appelle la fonction api_add_comment
-            o_comment_interface.api_add_comment({"text": "comment"})
+            o_sharing_interface.api_add_sharings(["_id"])
             # Vérification sur o_mock_request
             o_mock_request.assert_called_once_with(
                 "store_entity_add_comment",
@@ -51,8 +51,8 @@ class SharingInterfaceTestCase(unittest.TestCase):
         # On mock la fonction request, on veut vérifier qu'elle est appelée avec les bons param
         with patch.object(o_api_requester, "route_request", return_value=o_response) as o_mock_request:
             # on appelle la fonction à tester :api_list_comments
-            o_comment_interface = CommentInterface({"_id": "id_entité"})
-            l_data_recupere = o_comment_interface.api_list_comments()
+            o_sharing_interface = SharingInterface({"_id": "id_entité"})
+            l_data_recupere = o_sharing_interface.api_list_sharings()
             # on vérifie que route_request est appelé correctement
             o_mock_request.assert_called_once_with(
                 "store_entity_list_comment",
@@ -63,3 +63,18 @@ class SharingInterfaceTestCase(unittest.TestCase):
 
     def test_api_remove_sharings(self) -> None:
         "Vérifie le bon fonctionnement de api_remove_sharings."
+        # Instanciation du ApiRequester
+        o_api_requester = ApiRequester()
+        # On mock la fonction request, on veut vérifier qu'elle est appelée avec les bons param
+        with patch.object(o_api_requester, "route_request", return_value=None) as o_mock_request:
+            # On effectue la suppression d'un commentaire
+            # On instancie une entité dont on va supprimer le partage de livraison
+            o_sharing_interface = SharingInterface({"_id": "id_entité"})
+            # On appelle la fonction api_remove_comment
+            o_sharing_interface.api_remove_sharings(["_id"])
+            # Vérification sur o_mock_request
+            o_mock_request.assert_called_once_with(
+                "store_entity_remove_sharings",
+                route_params={"store_entity": "id_entité", "last_event": "id_entité"},
+                method=ApiRequester.DELETE,
+            )
