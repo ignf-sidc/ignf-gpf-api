4a3829edf98f71b58af0272ed1877ed0b934ec32
Ludivine Schlegel <ludivine.schlegel@ign.fr>
2022-06-23 14:23:23 +0200

    GpfTestCase: ajout tearDownClass

--- tests/GpfTestCase.py
+++ tests/GpfTestCase.py
@@ -7,15 +7,23 @@ from ignf_gpf_api.io.Config import Config
 class GpfTestCase(unittest.TestCase):
     """classe générique pour les tests"""
 
+    _o_patch_om = None
     _o_mock_om = None
 
     @classmethod
     def setUpClass(cls) -> None:
-        """fonction lancer avant les testes sur la classe
-
-        -> mock de om pour supprimé l'affichage
+        """fonction lancée une fois avant tous les tests de la classe
+        -> mock de om pour supprimer l'affichage
         """
         print("setup")
-        o_el = patch.object(Config, "om", new_callable=PropertyMock)
+        cls._o_patch_om = o_el = patch.object(Config, "om", new_callable=PropertyMock, return_value=MagicMock())
         cls._o_mock_om = o_el.start()
-        cls._o_mock_om.return_value = MagicMock()
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        """fonction lancée une fois après tous les tests de la classe
+        -> dé-mock de om pour stopper la suppression l'affichage
+        """
+        # On ne mock plus la classe d'authentification
+        if cls._o_patch_om:
+            cls._o_patch_om.stop()
