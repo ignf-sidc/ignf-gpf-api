3d6caf4cc508a3f7fd54dfa09b732afbb047ead8
Ludivine Schlegel <ludivine.schlegel@ign.fr>
2022-06-29 10:33:55 +0200

    ajout : PartialEditInterfaceTestCase

---
+++ tests/store/interface/PartialEditInterfaceTestCase.py
@@ -0,0 +1,11 @@
+from unittest.mock import patch
+import requests
+import requests_mock
+
+from ignf_gpf_api.io.ApiRequester import ApiRequester
+from ignf_gpf_api.store.interface.PartialEditInterface import PartialEditInterface
+from tests.GpfTestCase import GpfTestCase
+
+
+class PartialEditInterfaceTestCase(GpfTestCase):
+    pass
