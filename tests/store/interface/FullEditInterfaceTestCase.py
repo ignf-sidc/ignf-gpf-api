f25955ec38c87d5494bbea11555a6801c54117e0
Ludivine Schlegel <ludivine.schlegel@ign.fr>
2022-06-29 10:35:00 +0200

    ajout : FullEditInterfaceTestCase

---
+++ tests/store/interface/FullEditInterfaceTestCase.py
@@ -0,0 +1,11 @@
+from unittest.mock import patch
+import requests
+import requests_mock
+
+from ignf_gpf_api.io.ApiRequester import ApiRequester
+from ignf_gpf_api.store.interface.FullEditInterface import FullEditInterface
+from tests.GpfTestCase import GpfTestCase
+
+
+class FullEditInterfaceTestCase(GpfTestCase):
+    pass
