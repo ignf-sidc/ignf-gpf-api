d4ef6dc520a5d1dcb8f1d9f5c9ec5808f1bb3b8f
fbacquelot <francois.bacquelot@ign.fr>
2022-06-30 11:03:01 +0200

    Update FullEditInterfaceTestCase.py
    Fonction test 'test_api_full_edit' de la classe 'FullEditInterface()' passe sans bugger avec le 'o_response' correct

--- tests/store/interface/FullEditInterfaceTestCase.py
+++ tests/store/interface/FullEditInterfaceTestCase.py
@@ -1,9 +1,8 @@
-from unittest.mock import patch
 import unittest
+from unittest.mock import patch
 import requests
 import requests_mock
 
-# from tests.GpfTestCase import GpfTestCase
 from ignf_gpf_api.store.StoreEntity import StoreEntity
 from ignf_gpf_api.store.interface.FullEditInterface import FullEditInterface
 from ignf_gpf_api.io.ApiRequester import ApiRequester
@@ -36,21 +35,18 @@ class FullEditInterfaceTestCase(unittest.TestCase):
 
         # Instanciation d'une fausse réponse HTTP
         with requests_mock.Mocker() as o_mock:
-            o_mock.post("http://test.com/", json=d_old_api_data)
-            o_response = requests.get("http://test.com/").text
+            o_mock.post("http://test.com/", json={"_id": "123456789"})
+            o_response = requests.request("POST", "http://test.com/")
 
         # Instanciation du ApiRequester
         o_api_requester = ApiRequester()
 
-        # On mock la fonction request, on veut vérifier qu'elle est appelée avec les bons paramètres
+        # On mock la fonction request, on veut vérifier qu'elle est appelée avec les bons param
         with patch.object(o_api_requester, "route_request", return_value=o_response) as o_mock_request:
             o_full_edit_interface = FullEditInterface(d_old_api_data)
-            # on fait appel à api_full_edit(d_old_api_data)
-            o_full_edit_interface.api_full_edit(d_old_api_data)
-            # On appelle la fonction api_update
-            o_full_edit_interface.api_update()
+            o_full_edit_interface.api_full_edit(d_full_modified_api_data)
             # Vérification sur o_mock_request
-            o_mock_request.assert_called_once_with("store_entity_get", route_params={"store_entity": "id_à_maj"})
-            # Vérification que les infos de l'entité sont modifiées complètement
-            o_store_entity = StoreEntity(d_old_api_data)
+            o_mock_request.assert_called_once_with("store_entity_full_edit", data=d_full_modified_api_data, method=ApiRequester.PUT, route_params={"store_entity": "123456789"})
+            # Vérification que les infos de l'entité sont maj
+            o_store_entity = StoreEntity(d_full_modified_api_data)
             self.assertDictEqual(o_store_entity.get_store_properties(), d_full_modified_api_data)
