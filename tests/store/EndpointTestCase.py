4bcbac521f33f349608e4173fcb77ce9cc49060e
Valentin Sasyan <valentin.sasyan@ign.fr>
2022-07-05 15:07:48 +0200

    Ajout classe Endpoint + modif StoreEntityResolver

---
+++ tests/store/EndpointTestCase.py
@@ -0,0 +1,77 @@
+from unittest.mock import patch
+import requests
+import requests_mock
+
+from ignf_gpf_api.io.ApiRequester import ApiRequester
+from ignf_gpf_api.store.Endpoint import Endpoint
+
+from tests.GpfTestCase import GpfTestCase
+
+
+class EndpointTestCase(GpfTestCase):
+    """Tests Endpoint class.
+
+    cmd : python3 -m unittest -b tests.store.EndpointTestCase
+    """
+
+    def test_api_list(self) -> None:
+        """Vérifie le bon fonctionnement de api_list."""
+
+        # Instanciation d'une fausse réponse HTTP
+        with requests_mock.Mocker(real_http=True) as o_mock:
+            o_mock.post(
+                "http://test.com/",
+                json={
+                    "endpoints": [
+                        {
+                            "_id": "endpoint_1",
+                            "name": "Service WMTS",
+                            "type": "WMTS-TMS",
+                        },
+                        {
+                            "_id": "endpoint_2",
+                            "name": "Service de téléchargement",
+                            "type": "DOWNLOAD",
+                        },
+                    ]
+                },
+            )
+            o_response = requests.request("POST", "http://test.com/")
+
+        # 1 : pas de filtres
+        # On mock la fonction request, on veut vérifier qu'elle est appelée avec les bons param
+        with patch.object(ApiRequester(), "route_request", return_value=o_response) as o_mock_request:
+            l_endpoints = Endpoint.api_list()
+            # on vérifie que route_request est appelée correctement
+            o_mock_request.assert_called_once_with("datastore_get")
+            # on vérifie qu'on a bien récupéré une liste de 2 Endpoints
+            self.assertIsInstance(l_endpoints, list)
+            self.assertEqual(len(l_endpoints), 2)
+            self.assertIsInstance(l_endpoints[0], Endpoint)
+            self.assertIsInstance(l_endpoints[1], Endpoint)
+            self.assertEqual(l_endpoints[0].id, "endpoint_1")
+            self.assertEqual(l_endpoints[1].id, "endpoint_2")
+
+        # 2 : filtre sur le nom
+        # On mock la fonction request, on veut vérifier qu'elle est appelée avec les bons param
+        with patch.object(ApiRequester(), "route_request", return_value=o_response) as o_mock_request:
+            l_endpoints = Endpoint.api_list(infos_filter={"name": "Service WMTS"})
+            # on vérifie que route_request est appelée correctement
+            o_mock_request.assert_called_once_with("datastore_get")
+            # on vérifie qu'on a bien récupéré une liste de 1 Endpoint
+            self.assertIsInstance(l_endpoints, list)
+            self.assertEqual(len(l_endpoints), 1)
+            self.assertIsInstance(l_endpoints[0], Endpoint)
+            self.assertEqual(l_endpoints[0].id, "endpoint_1")
+
+        # 2 : filtre sur le type
+        # On mock la fonction request, on veut vérifier qu'elle est appelée avec les bons param
+        with patch.object(ApiRequester(), "route_request", return_value=o_response) as o_mock_request:
+            l_endpoints = Endpoint.api_list(infos_filter={"type": "DOWNLOAD"})
+            # on vérifie que route_request est appelée correctement
+            o_mock_request.assert_called_once_with("datastore_get")
+            # on vérifie qu'on a bien récupéré une liste de 1 Endpoint
+            self.assertIsInstance(l_endpoints, list)
+            self.assertEqual(len(l_endpoints), 1)
+            self.assertIsInstance(l_endpoints[0], Endpoint)
+            self.assertEqual(l_endpoints[0].id, "endpoint_2")
