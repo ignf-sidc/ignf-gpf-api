a0fbe741f895b56d787f35ff3b29d76adf0585c0
Valentin Sasyan <valentin.sasyan@ign.fr>
2022-07-27 17:58:38 +0200

    UserResolver : récupération à l'instanciation

--- ignf_gpf_api/workflow/resolver/UserResolver.py
+++ ignf_gpf_api/workflow/resolver/UserResolver.py
@@ -1,41 +1,34 @@
 from typing import Any, Dict
+
+from ignf_gpf_api.io.ApiRequester import ApiRequester
 from ignf_gpf_api.workflow.resolver.AbstractResolver import AbstractResolver
 from ignf_gpf_api.workflow.resolver.Errors import ResolveDataUsernameNotFound
 
 
 class UserResolver(AbstractResolver):
     """Classe permettant de récupérer les informations sur l'utilisateur authentifié.
+    La plu-value sur la classe DictRevolver est que les info de l'utilisateur sont
+    directement récupérées par le constructeur de la classe.
 
     Attributes :
         __name (str): nom de code du resolver
         __username_data (Dict[str, Any]): liste des infos de l'utilisateur authentifié
     """
 
-    def __init__(self, name: str, username_data: Dict[str, Any]) -> None:
-        """Constructeur.
-
-                Args:
-                    name (str): nom du résolveur
-                    username_data (Dict[str, Any]): liste de clé/valeur à
-        utiliser
-        """
-        super().__init__(name)
-        self.__username_data: Dict[str, Any] = username_data
-
-    def resolve(self, string_to_solve: str) -> Any:
-        """résoudre la chaîne de caractères pour récupérer les infos de l'utilisateur
+    def __init__(self, name: str) -> None:
+        """Constructeur. Récupération des infos de l'utilisateur via l'API.
 
         Args:
-            string_to_solve (str): chaîne de caractères à résoudre
-
-        Raises:
-            ResolveDataUsernameNotFound: gérer l'erreur quand l'info de l'utilisateur n'est pas récupérée
-
-        Returns:
-            str: chaîne de caractères résolue
+            name (str): nom du résolveur
         """
+        super().__init__(name)
+        # On récupère les infos sur l'API
+        o_response = ApiRequester().route_request("user_get")
+        self.__user_data: Dict[str, Any] = o_response.json()
+
+    def resolve(self, string_to_solve: str) -> Any:
         # La chaîne à résoudre est en fait la clé, donc il suffit de renvoyer la valeur associée
-        if string_to_solve in self.__username_data:
-            return str(self.__username_data[string_to_solve])
+        if string_to_solve in self.__user_data:
+            return str(self.__user_data[string_to_solve])
         # Sinon on lève une exception
         raise ResolveDataUsernameNotFound(self.name, string_to_solve)
