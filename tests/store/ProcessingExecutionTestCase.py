0199b5310cd7e665c1616f02748a24ffd7918545
Valentin Sasyan <valentin.sasyan@ign.fr>
2022-09-20 18:09:52 +0200

    ProcessingExecution : creation

--- tests/store/ProcessingExecutionTestCase.py
+++ tests/store/ProcessingExecutionTestCase.py
@@ -61,6 +61,19 @@ class ProcessingExecutionTestCase(GpfTestCase):
                 method=ApiRequester.POST,
             )
 
+    def test_creation(self) -> None:
+        """Vérifie le bon fonctionnement de creation."""
+        # Instanciation
+        o_processing_execution = ProcessingExecution({"_id": "id_entité"})
+        # On mock la fonction route_request, on veut vérifier qu'elle est appelée avec les bons params
+        with patch.object(o_processing_execution, "_get_datetime", return_value=datetime.now()) as o_mock_get_datetime:
+            # on appelle la fonction à tester : creation
+            o_datetime = o_processing_execution.creation
+
+            # on vérifie que route_request est appelé correctement
+            o_mock_get_datetime.assert_called_once_with("creation")
+            self.assertEqual(o_datetime, o_mock_get_datetime.return_value)
+
     def test_launch(self) -> None:
         """Vérifie le bon fonctionnement de launch."""
         # Instanciation
