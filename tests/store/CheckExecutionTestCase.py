e9f916fc39a56798c69c8ecaa205c168a3024364
Ludivine Schlegel <ludivine.schlegel@ign.fr>
2023-07-07 15:44:11 +0200

    Amélioration affichage des logs

--- tests/store/CheckExecutionTestCase.py
+++ tests/store/CheckExecutionTestCase.py
@@ -14,16 +14,24 @@ class CheckExecutionTestCase(GpfTestCase):
     def test_api_logs(self) -> None:
         "Vérifie le bon fonctionnement de api_logs."
         s_data = "2022/05/18 14:29:25       INFO §USER§ Envoi du signal de début de l'exécution à l'API.\n2022/05/18 14:29:25       INFO §USER§ Signal transmis avec succès."
-        o_response = GpfTestCase.get_response(text=s_data)
-        # On mock la fonction route_request, on veut vérifier qu'elle est appelée avec les bons params
-        with patch.object(ApiRequester, "route_request", return_value=o_response) as o_mock_request:
-            # on appelle la fonction à tester : api_logs
-            o_check_execution = CheckExecution({"_id": "id_entité"})
-            s_data_recupere = o_check_execution.api_logs()
-            # on vérifie que route_request est appelé correctement
-            o_mock_request.assert_called_once_with(
-                "check_execution_logs",
-                route_params={"check_execution": "id_entité"},
-            )
-            # on vérifie la similitude des données retournées
-            self.assertEqual(s_data, s_data_recupere)
+        l_rep = [
+            {"data": s_data, "rep": s_data},
+            {"data": "", "rep": ""},
+            {"data": "[]", "rep": ""},
+            {"data": '["log1", "log2", " log \\"complexe\\""]', "rep": 'log1\nlog2\n log "complexe"'},
+        ]
+
+        for d_rep in l_rep:
+            o_response = GpfTestCase.get_response(text=d_rep["data"])
+            # On mock la fonction route_request, on veut vérifier qu'elle est appelée avec les bons params
+            with patch.object(ApiRequester, "route_request", return_value=o_response) as o_mock_request:
+                # on appelle la fonction à tester : api_logs
+                o_check_execution = CheckExecution({"_id": "id_entité"})
+                s_data_recupere = o_check_execution.api_logs()
+                # on vérifie que route_request est appelé correctement
+                o_mock_request.assert_called_once_with(
+                    "check_execution_logs",
+                    route_params={"check_execution": "id_entité"},
+                )
+                # on vérifie la similitude des données retournées
+                self.assertEqual(d_rep["rep"], s_data_recupere)
--- tests/store/ProcessingExecutionTestCase.py
+++ tests/store/ProcessingExecutionTestCase.py
@@ -15,21 +15,29 @@ class ProcessingExecutionTestCase(GpfTestCase):
     def test_api_logs(self) -> None:
         """Vérifie le bon fonctionnement de api_logs."""
         s_data = "2022/05/18 14:29:25       INFO §USER§ Envoi du signal de début de l'exécution à l'API.\n2022/05/18 14:29:25       INFO §USER§ Signal transmis avec succès."
-        # Instanciation d'une fausse réponse HTTP
-        o_response = GpfTestCase.get_response(text=s_data)
-        # On mock la fonction route_request, on veut vérifier qu'elle est appelée avec les bons params
-        with patch.object(ApiRequester, "route_request", return_value=o_response) as o_mock_request:
-            # on appelle la fonction à tester : api_logs
-            o_processing_execution = ProcessingExecution({"_id": "id_entité"})
-            s_data_recupere = o_processing_execution.api_logs()
+        l_rep = [
+            {"data": s_data, "rep": s_data},
+            {"data": "", "rep": ""},
+            {"data": "[]", "rep": ""},
+            {"data": '["log1", "log2", " log \\"complexe\\""]', "rep": 'log1\nlog2\n log "complexe"'},
+        ]
 
-            # on vérifie que route_request est appelé correctement
-            o_mock_request.assert_called_once_with(
-                "processing_execution_logs",
-                route_params={"processing_execution": "id_entité"},
-            )
-            # on vérifie la similitude des données retournées
-            self.assertEqual(s_data, s_data_recupere)
+        for d_rep in l_rep:
+            # Instanciation d'une fausse réponse HTTP
+            o_response = GpfTestCase.get_response(text=d_rep["data"])
+            # On mock la fonction route_request, on veut vérifier qu'elle est appelée avec les bons params
+            with patch.object(ApiRequester, "route_request", return_value=o_response) as o_mock_request:
+                # on appelle la fonction à tester : api_logs
+                o_processing_execution = ProcessingExecution({"_id": "id_entité"})
+                s_data_recupere = o_processing_execution.api_logs()
+
+                # on vérifie que route_request est appelé correctement
+                o_mock_request.assert_called_once_with(
+                    "processing_execution_logs",
+                    route_params={"processing_execution": "id_entité"},
+                )
+                # on vérifie la similitude des données retournées
+                self.assertEqual(d_rep["rep"], s_data_recupere)
 
     def test_api_launch(self) -> None:
         """Vérifie le bon fonctionnement de api_launch."""
