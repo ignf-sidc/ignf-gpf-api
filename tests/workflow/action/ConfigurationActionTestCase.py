f32476965eab35fdb0b2b4f9745cbcff564f2d4c
Ludivine Schlegel <ludivine.schlegel@ign.fr>
2023-07-31 14:25:51 +0200

    ConfigurationAction: gestion des commentaires en doublon en cas de reprise.

--- tests/workflow/action/ConfigurationActionTestCase.py
+++ tests/workflow/action/ConfigurationActionTestCase.py
@@ -46,19 +46,28 @@ class ConfigurationActionTestCase(GpfTestCase):
                 o_mock_api_list.assert_called_once_with(infos_filter={"info": "val"}, tags_filter={"tag": "val"})
                 self.assertEqual(o_stored_data, o_c1)
 
-    def run_args(self, tags: Optional[Dict[str, Any]], comments: Optional[List[str]], config_already_exists: bool) -> None:
+    def run_args(
+        self,
+        tags: Optional[Dict[str, Any]],
+        comments: Optional[List[str]],
+        config_already_exists: bool,
+        comment_exist: bool = False,
+    ) -> None:
         """lancement +test de ConfigurationAction.run selon param
         Args:
             tags (Optional[Dict[str, Any]]): dict des tags ou None
             comments (Optional[List[str]]): liste des comments ou None
             config_already_exists (bool): configuration déjà existante
+            comment_exist (bool): si on a un commentaire qui existe déjà
         """
         # creation du dictionnaire qui reprend les paramètres du workflow pour créer une configuration
-        d_action = {"type": "configuration", "body_parameters": {"param": "valeur"}}
+        d_action: Dict[str, Any] = {"type": "configuration", "body_parameters": {"param": "valeur"}}
         if tags is not None:
             d_action["tags"] = tags
         if comments is not None:
-            d_action["comments"] = comments
+            d_action["comments"] = comments.copy()
+            if comment_exist:
+                d_action["comments"].append("commentaire existe")
 
         # initialisation de Configuration
         o_conf = ConfigurationAction("contexte", d_action)
@@ -66,6 +75,8 @@ class ConfigurationActionTestCase(GpfTestCase):
         # mock de configuration
         o_mock_configuration = MagicMock()
         o_mock_configuration.api_launch.return_value = None
+        o_mock_configuration.api_add_comment.return_value = None
+        o_mock_configuration.api_list_comments.return_value = [{"text": "commentaire existe"}] if comment_exist else []
 
         # Liste des configurations déjà existantes
         if config_already_exists:
@@ -95,9 +106,10 @@ class ConfigurationActionTestCase(GpfTestCase):
                     o_mock_configuration.api_add_tags.assert_not_called()
 
                 # test commentaires
-                if "comments" in d_action and d_action["comments"]:
-                    self.assertEqual(len(d_action["comments"]), o_mock_configuration.api_add_comment.call_count)
-                    for s_comm in d_action["comments"]:
+                if "comments" in d_action and comments:
+                    o_mock_configuration.api_list_comments.assert_called_once_with()
+                    self.assertEqual(len(comments), o_mock_configuration.api_add_comment.call_count)
+                    for s_comm in comments:
                         o_mock_configuration.api_add_comment.assert_any_call({"text": s_comm})
                 else:
                     o_mock_configuration.api_add_comment.assert_not_called()
@@ -107,10 +119,12 @@ class ConfigurationActionTestCase(GpfTestCase):
         # On teste avec et sans configuration renvoyé par api_list
         for b_config_already_exists in [True, False]:
             ## sans tag + sans commentaire
-            self.run_args(None, None, b_config_already_exists)
+            self.run_args(None, None, b_config_already_exists, False)
             ## tag vide + commentaire vide
-            self.run_args({}, [], b_config_already_exists)
+            self.run_args({}, [], b_config_already_exists, False)
             ## 1 tag + 1 commentaire
-            self.run_args({"tag1": "val1"}, ["comm1"], b_config_already_exists)
+            self.run_args({"tag1": "val1"}, ["comm1"], b_config_already_exists, False)
             ## 2 tag + 4 commentaire
-            self.run_args({"tag1": "val1", "tag2": "val2"}, ["comm1", "comm2", "comm3", "comm4"], b_config_already_exists)
+            self.run_args({"tag1": "val1", "tag2": "val2"}, ["comm1", "comm2", "comm3", "comm4"], b_config_already_exists, False)
+            ## 2 tag + 4 commentaire + 1 commentaire qui existe déjà
+            self.run_args({"tag1": "val1", "tag2": "val2"}, ["comm1", "comm2", "comm3", "comm4"], b_config_already_exists, True)
