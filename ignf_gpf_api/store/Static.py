22077a6be7562fd014738c4698995bc7fa54ceb6
Ludivine Schlegel <ludivine.schlegel@ign.fr>
2023-08-09 16:37:33 +0200

    Static: utilisation de DownloadInterface

--- ignf_gpf_api/store/Static.py
+++ ignf_gpf_api/store/Static.py
@@ -1,13 +1,11 @@
-from pathlib import Path
-from typing import Optional
-
 from ignf_gpf_api.store.StoreEntity import StoreEntity
 from ignf_gpf_api.store.interface.TagInterface import TagInterface
 from ignf_gpf_api.store.interface.PartialEditInterface import PartialEditInterface
 from ignf_gpf_api.store.interface.FullEditInterface import FullEditInterface
+from ignf_gpf_api.store.interface.DownloadInterface import DownloadInterface
 
 
-class Static(TagInterface, PartialEditInterface, FullEditInterface, StoreEntity):
+class Static(DownloadInterface, TagInterface, PartialEditInterface, FullEditInterface, StoreEntity):
     """Classe Python représentant l'entité Fichier statique (static).
 
     Cette classe permet d'effectuer les actions spécifiques liées aux fichiers statiques : création,
@@ -21,12 +19,3 @@ class Static(TagInterface, PartialEditInterface, FullEditInterface, StoreEntity)
     TYPE_GEOSERVER_STYLE = "GEOSERVER-STYLE"
     TYPE_ROK4_STYLE = "ROK4-STYLE"
     TYPE_DERIVATION_SQL = "DERIVATION-SQL"
-
-    def api_download(self, file_path: Path, datastore: Optional[str] = None) -> None:
-        """Télécharge le Fichier Statique et l'enregistre localement.
-
-        Args:
-            file_path: chemin local où enregistrer le fichier
-            datastore (Optional[str]): id du datastore à utiliser. Si None, le datastore sera récupéré dans configuration. Defaults to None.
-        """
-        raise NotImplementedError("Fonction « Téléchargement fichier statique » non implémentée.")
