a7eee00f7d7cf928a000c861c593e616d76ee885
Ludivine Schlegel <ludivine.schlegel@ign.fr>
2023-08-10 09:34:04 +0200

    CreatedByUploadFileInterface: dev

---
+++ ignf_gpf_api/store/interface/CreatedByUploadFileInterface.py
@@ -0,0 +1,53 @@
+from pathlib import Path
+from typing import Any, Dict, Optional, Type, TypeVar
+from ignf_gpf_api.io.ApiRequester import ApiRequester
+from ignf_gpf_api.io.Config import Config
+from ignf_gpf_api.store.StoreEntity import StoreEntity
+
+T = TypeVar("T", bound="StoreEntity")
+
+
+class CreatedByUploadFileInterface(StoreEntity):
+    """Interface de StoreEntity pour gérer les entités créer par l'upload d'un fichier."""
+
+    @classmethod
+    def api_create(cls: Type[T], data: Optional[Dict[str, Any]], route_params: Optional[Dict[str, Any]] = None) -> T:
+        """Crée une nouvelle entité dans l'API.
+
+        Args:
+            data: Données nécessaires pour la création. Donc "file": Path("chemin fichier") et "api_path": "nom fichier"
+            route_params: Paramètres de résolution de la route.
+
+        Returns:
+            (StoreEntity): Entité créée
+        """
+        s_datastore: Optional[str] = None
+        # Test du dictionnaire route_params
+        if isinstance(route_params, dict) and "datastore" in route_params:
+            s_datastore = route_params.get("datastore")
+
+        # Génération du nom de la route
+        s_route = f"{cls._entity_name}_upload"
+
+        # récupération du ficher et du nom du ficher après livraison
+        if not data or "file" not in data or not Path(data["file"]).exists() or "api_path" not in data:
+            raise Exception('Entité créer par l\'upload d\'un fichier, les clefs "file": Path("chemin fichier") et "api_path": "nom fichier" sont obligatoires dans data')
+        p_file = Path(data.pop("file"))
+        s_api_path = data.pop("api_path")
+
+        # nom de la clef dans le fichier
+        s_file_key = Config().get_str("upload", "push_data_file_key")
+
+        # Requête
+        o_response = ApiRequester().route_upload_file(
+            s_route,
+            p_file,
+            s_file_key,
+            route_params=route_params,
+            params={"path": s_api_path},
+            method=ApiRequester.POST,
+            data=data,
+        )
+
+        # Instanciation
+        return cls(o_response.json(), datastore=s_datastore)
