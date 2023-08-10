fdaaa9c9359bc6bcb37021ecb4c53cf9dfb573a1
Ludivine Schlegel <ludivine.schlegel@ign.fr>
2023-08-10 13:26:22 +0200

    Annexes : dev + tests

---
+++ ignf_gpf_api/store/Annexes.py
@@ -0,0 +1,70 @@
+from typing import List, Optional
+import requests
+
+from ignf_gpf_api.io.ApiRequester import ApiRequester
+from ignf_gpf_api.store.StoreEntity import StoreEntity
+from ignf_gpf_api.store.interface.PartialEditInterface import PartialEditInterface
+from ignf_gpf_api.store.interface.ReUploadFileInterface import ReUploadFileInterface
+from ignf_gpf_api.store.interface.DownloadInterface import DownloadInterface
+from ignf_gpf_api.store.interface.CreatedByUploadFileInterface import CreatedByUploadFileInterface
+
+
+class Annexes(CreatedByUploadFileInterface, DownloadInterface, PartialEditInterface, ReUploadFileInterface, StoreEntity):
+    """Classe Python représentant l'entité Fichier statique (annexes).
+
+    Cette classe permet d'effectuer les actions spécifiques liées aux fichiers statiques : création,
+    remplacement, mise à jour, suppression.
+    """
+
+    _entity_name = "annexes"
+    _entity_title = "annexes"
+
+    @staticmethod
+    def publish_by_label(labels: List[str], datastore: Optional[str] = None) -> int:
+        """publication de toutes les annexes portent l'ensemble de label
+
+        Args:
+            labels (List[str]): liste des labels
+            datastore (Optional[str], optional): Identifiant du datastore
+
+        Returns:
+            int: nombre d'annexes publier
+        """
+
+        # Génération du nom de la route
+        s_route = f"{Annexes._entity_name}_publish_by_label"
+
+        # Requête
+        o_response: requests.Response = ApiRequester().route_request(
+            s_route,
+            route_params={"datastore": datastore},
+            params={"labels": labels},
+            method=ApiRequester.POST,
+        )
+
+        return int(o_response.text)
+
+    @staticmethod
+    def unpublish_by_label(labels: List[str], datastore: Optional[str] = None) -> int:
+        """dépublication de toutes les annexes portent l'ensemble de label
+
+        Args:
+            labels (List[str]): liste des labels
+            datastore (Optional[str], optional): Identifiant du datastore
+
+        Returns:
+            int: nombre d'annexes de dépublier
+        """
+
+        # Génération du nom de la route
+        s_route = f"{Annexes._entity_name}_unpublish_by_label"
+
+        # Requête
+        o_response: requests.Response = ApiRequester().route_request(
+            s_route,
+            route_params={"datastore": datastore},
+            params={"labels": labels},
+            method=ApiRequester.POST,
+        )
+
+        return int(o_response.text)
---
+++ tests/store/AnnexesTestCase.py
@@ -0,0 +1,58 @@
+from unittest.mock import patch
+
+from ignf_gpf_api.io.ApiRequester import ApiRequester
+from ignf_gpf_api.store.Annexes import Annexes
+from tests.GpfTestCase import GpfTestCase
+
+
+class AnnexesTestCase(GpfTestCase):
+    """Tests Annexes class.
+
+    cmd : python3 -m unittest -b tests.store.AnnexesTestCase
+    """
+
+    def test_publish_by_label(self) -> None:
+        "Vérifie le bon fonctionnement de publish_by_label."
+        i_file = 10
+        l_labels = ["a", "b", "c"]
+
+        for s_datastore in [None, "publish_by_label"]:
+            # On mock la fonction route_request, on veut vérifier qu'elle est appelée avec les bons params
+            o_response = GpfTestCase.get_response(text=f"{i_file}")
+            with patch.object(ApiRequester, "route_request", return_value=o_response) as o_mock_request:
+
+                # on appelle la fonction à tester : publish_by_label
+                s_data_recupere = Annexes.publish_by_label(l_labels, s_datastore)
+
+                # on vérifie que route_request est appelé correctement
+                o_mock_request.assert_called_once_with(
+                    "annexes_publish_by_label",
+                    route_params={"datastore": s_datastore},
+                    params={"labels": l_labels},
+                    method=ApiRequester.POST,
+                )
+                # on vérifie la similitude des données retournées
+                self.assertEqual(i_file, s_data_recupere)
+
+    def test_unpublish_by_label(self) -> None:
+        "Vérifie le bon fonctionnement de unpublish_by_label."
+        i_file = 10
+        l_labels = ["a", "b", "c"]
+
+        for s_datastore in [None, "unpublish_by_label"]:
+            # On mock la fonction route_request, on veut vérifier qu'elle est appelée avec les bons params
+            o_response = GpfTestCase.get_response(text=f"{i_file}")
+            with patch.object(ApiRequester, "route_request", return_value=o_response) as o_mock_request:
+
+                # on appelle la fonction à tester : unpublish_by_label
+                s_data_recupere = Annexes.unpublish_by_label(l_labels, s_datastore)
+
+                # on vérifie que route_request est appelé correctement
+                o_mock_request.assert_called_once_with(
+                    "annexes_unpublish_by_label",
+                    route_params={"datastore": s_datastore},
+                    params={"labels": l_labels},
+                    method=ApiRequester.POST,
+                )
+                # on vérifie la similitude des données retournées
+                self.assertEqual(i_file, s_data_recupere)
